c3f48a43368d31a2c4718658abc9605ef838c5c6
Madis Bobrovski <115641439+madisbobo@users.noreply.github.com>
2023-08-22 11:05:38 +0300

    Build a responsive layout

--- game_discovery_app/src/App.tsx
+++ game_discovery_app/src/App.tsx
@@ -1,10 +1,38 @@
 import { useState } from "react";
-import { Heading, Text } from "@chakra-ui/react";
+import {
+  Heading,
+  Text,
+  SimpleGrid,
+  Box,
+  Grid,
+  GridItem,
+  Show,
+} from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Heading as="h3">Hello World!</Heading>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main" "footer"`,
+          lg: `"nav nav" "aside main" "footer footer"`,
+        }}
+      >
+        <GridItem area="nav" bg="yellow.100">
+          nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="green.100">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="blue.100">
+          main
+        </GridItem>
+        <GridItem area="footer" bg="purple.100">
+          footer
+        </GridItem>
+      </Grid>
     </>
   );
 }
