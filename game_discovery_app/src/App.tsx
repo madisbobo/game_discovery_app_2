7808e49a3b8c1dc35494cd51fd2419192d7d8521
Madis Bobrovski <115641439+madisbobo@users.noreply.github.com>
2023-08-22 09:22:10 +0300

    Game discovery app initial commit

---
+++ game_discovery_app/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { Heading, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Heading as="h3">Hello World!</Heading>
+    </>
+  );
+}
+
+export default App;
