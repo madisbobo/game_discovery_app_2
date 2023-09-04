3ef50bbf5c8fc234c0a1391ce3f08b2e927db522
Madis Bobrovski <115641439+madisbobo@users.noreply.github.com>
2023-09-04 12:07:06 +0300

    Fixing the no image situation

--- game_discovery_app/src/services/image-url.ts
+++ game_discovery_app/src/services/image-url.ts
@@ -1,6 +1,7 @@
+import noImage from "../assets/no-image-placeholder.webp"
 
 const imgCropper = (url: string) => {
-    if (!url) return "";
+    if (!url) return noImage;
     let modUrl = url.split("/media/")
     modUrl.splice(1, 0, "media/crop/600/400")
     return modUrl.join("/")
