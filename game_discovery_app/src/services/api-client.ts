0b6de59236fa21551302e40c3c7f22544ce87f3f
Madis Bobrovski <115641439+madisbobo@users.noreply.github.com>
2023-09-06 14:50:17 +0300

    deployment

--- game_discovery_app/src/services/api-client.ts
+++ game_discovery_app/src/services/api-client.ts
@@ -3,8 +3,9 @@ import axios, { CanceledError } from "axios";
 const apiClient = axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: "8560a6cb69ab4861b918e8577d1ee4fa"
+        key: import.meta.env.VITE_REACT_APP_API_KEY
     }
 })
+
 
 export {CanceledError, apiClient};
