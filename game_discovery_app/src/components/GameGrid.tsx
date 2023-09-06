8e42c66d7a06bfb828bdec29946316cd3926540f
Madis Bobrovski <115641439+madisbobo@users.noreply.github.com>
2023-09-06 11:03:01 +0300

    Refactoring error in GameGrid

--- game_discovery_app/src/components/GameGrid.tsx
+++ game_discovery_app/src/components/GameGrid.tsx
@@ -5,26 +5,22 @@ import GameCard from "./GameCard";
 import LoadingGrid from "./GameCardSkeleton";
 
 interface Props {
-  gameQuery: GameQuery
-
+  gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
   const { games, isLoading, error } = useGames(gameQuery);
-
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
-        {isLoading &&
-          skeletons.map((skeleton) => <LoadingGrid key={skeleton} />)}
-        {games.map((game) => (
-          <GameCard game={game} key={game.id} />
-        ))}
-      </SimpleGrid>
-    </>
+    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
+      {isLoading && skeletons.map((skeleton) => <LoadingGrid key={skeleton} />)}
+      {games.map((game) => (
+        <GameCard game={game} key={game.id} />
+      ))}
+    </SimpleGrid>
   );
 };
 
